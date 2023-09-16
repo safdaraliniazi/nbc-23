0f46a3f42fb6e37214984011a8dbc7e2451699eb
Safdar <787alisniazi787@gmail.com>
2023-09-16 20:58:24 +0530

    update organising profs

--- src/components/Organising.js
+++ src/components/Organising.js
@@ -113,8 +113,50 @@ const Organising = () => {
         class=" text-center"
         style={{ display: 'flex', flexDirection : 'column', fontSize: "1.3rem"  , color: 'black' , justifyContent : 'center' , alignItems : 'center'}}
       >
-        All Faculties of Department of Biotechnology and Medical Engineering,
-        NIT Rourkela. <br /> Student Co-ordinators:{" "}
+       <strong> All Faculties of Department of Biotechnology and Medical Engineering,
+        NIT Rourkela.</strong> <br /> {" "}
+
+        <br />
+
+         <div class="container d-flex flex-column flex-sm-row  justify-content-evenly">
+      <ul class="list-group w-50 w-100">
+        <li class="list-group-item">Prof M.K Gupta</li>
+        <li class="list-group-item">Prof Subhankar Paul</li>
+        <li class="list-group-item">Prof A. Thirugnanum</li>
+        <li class="list-group-item">Prof Devendra Verma</li>
+        <li class="list-group-item">Prof Amit Biswas</li>
+        <li class="list-group-item">Prof B.P Nayak</li>
+        <li class="list-group-item">Prof P. Balasubramanian</li>
+        <li class="list-group-item">Prof Angana Sarkar</li>
+        <li class="list-group-item">Prof Kasturi Dutta</li>
+        <li class="list-group-item h-100">  </li>
+      </ul>
+
+      <ul class="list-group w-50 w-100">
+        <li class="list-group-item">Prof Sivaraman J</li>
+        <li class="list-group-item">Prof Anju R. Babu</li>
+        <li class="list-group-item">Prof Bala Chakravarthy Neelapu</li>
+        <li class="list-group-item">Prof Earu Banoth</li>
+        <li class="list-group-item">Prof Mirza Khalid Baig</li>
+        <li class="list-group-item">Prof Prasoon Kumar</li>
+        <li class="list-group-item">Prof Ravi Kant Avvari</li>
+        <li class="list-group-item">Prof Amrita Singh</li>
+        <li class="list-group-item">Prof Anamika Singh</li>
+        <li class="list-group-item">Prof Lohit Kumar Srinivas Gujjala</li>
+   
+      </ul>
+    </div>
+
+        <ul class = "list-group w-50 w-75">
+        
+        </ul>
+
+        <br />
+        <br />
+
+    Student Co-ordinators:
+
+
             <ul className="std-co">
                 <li>Amit Mitra</li>
                 <li>Debasmita Chakraborty</li>
